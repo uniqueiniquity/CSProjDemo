3f1441d0ca35cd923264303d1c1485741ae7e828
Ben Lichtman <belichtm@microsoft.com>
2021-05-05 09:59:16 -0700

    Add project files.

---
+++ DiagnosticToolsDemo/Stub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiagnosticToolsDemo
+{
+    public class Stub
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ DiagnosticToolsDemo/index.ts
@@ -0,0 +1,6 @@
+﻿import { scaleLinear } from "d3-scale";
+
+let newLocal: string = "hello!";
+console.log(newLocal);
+const sl = scaleLinear([0, 1]);
+console.log(sl);
